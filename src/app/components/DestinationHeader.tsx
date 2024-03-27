77b790318d989451b30ac974d9f74628fbf3bd7c
chandan7dec <ck.kislay@gmail.com>
2024-03-27 10:23:34 +0530

    deploy to github pate

--- src/app/components/DestinationHeader.tsx
+++ src/app/components/DestinationHeader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const DestinationHeader = () => {
   return (
     <div className=' p-4   text-center '>
-      <h4 className=' text-success '>Distination List</h4>
+      <h4 className=' text-success '>Destination List</h4>
     </div>
   )
 }
